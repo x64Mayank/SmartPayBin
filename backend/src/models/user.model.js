0bff35acfe5306cccfc393d5e6f292c74f9ba395
Mayank Kumar <mayank.890.mk@gmail.com>
2026-03-29 20:07:25 +0530

    refactor: streamline password hashing logic and clean up CORS origin declaration

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -31,11 +31,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   this.password = await bcrypt.hash(this.password, 10);
-  next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
@@ -56,6 +55,7 @@ userSchema.methods.generateAccessToken = function () {
     }
   );
 };
+
 userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
