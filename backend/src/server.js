948b66194c10198dec585370026b372166357fc9
Mayank Kumar <mayank.890.mk@gmail.com>
2026-04-01 01:19:15 +0530

    feat: allow multiple CORS origins by splitting environment variable string into an array

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN.split(',').map(o => o.trim()),
     credentials: true,
   }),
 );
